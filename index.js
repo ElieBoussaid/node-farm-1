c3b14e2f4d1d5d76076391692a5b9d594b57d869
bridealseiny <mehdi.boussaid.free@gmail.com>
2022-01-30 12:32:15 +0100

    suite

--- index.js
+++ index.js
@@ -23,18 +23,29 @@ const url = require('url');
 
 // This an Array of products as Objects 
 const productsDataJs = JSON.parse(productsDataJSON);
+const productsIDs = productsDataJs.map(productObject => productObject.id ); 
 
 
 
 
 const templateFiller = function(productId) {
 
+  let notOrganic = ''; 
+
+
+  if (productsDataJs[productId].organic === true) { 
+    notOrganic = '';
+  } else { 
+    notOrganic = 'not-organic';
+  };
+
+
 const filledProductTemplate = productTemplate
                                              .replaceAll('{#ProductImage}', productsDataJs[productId].image)
                                              .replaceAll('{#ProductPrice}' , productsDataJs[productId].price)
-                                             .replaceAll('{#ProductDescription}' , productsDataJs[productId].description)  
+                                             .replaceAll('{#ProductDescription}' , productsDataJs[productId].description)
+                                             .replaceAll('{#not-organic}' , notOrganic)  
                                              ;
-
 
 return filledProductTemplate;
 
@@ -53,11 +64,13 @@ const server = http.createServer((request , response) => {
 
     const requestedUrl = request.url; 
     const parsedURrl = url.parse(requestedUrl , true); 
-    const requestedId = parsedURrl.query.id;
-   
+    const requestedId = ((parsedURrl.query.id)*1);
+    
+      
 
     
      
+
 
     
    
@@ -78,21 +91,33 @@ const server = http.createServer((request , response) => {
   // Products Routes
     else if ( parsedURrl.pathname === '/product'  ) {
 
-        response.writeHead(200, {
+     
+      response.writeHead(200, {
 
             'Content-Type': 'text/html' 
             
           }); 
           
+  
         
-       
-    
+     if (productsIDs.includes(requestedId)) {
+
+      response.end(templateFiller(requestedId));
+
+      } else {
+
+
+        response.end('<h1> Product Not found </h1>');
+     
+
+      }
           
-        response.end(templateFiller(requestedId));
+        
 
     }
+// FIN - Products Routes
 
-   
+
 // Not Found Route
     else {
 
@@ -116,6 +141,8 @@ const server = http.createServer((request , response) => {
 // Start server : 
 server.listen(8000 , '127.0.0.1' , () => { console.log('Server started and listening');
 });
+
+
 
 
 
